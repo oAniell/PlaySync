45a4ccc2d5fd68fe1de1c735309983e8375db744
Guilherme Aniel <guilherme_a_oliveir2@estudante.sc.senai.br>
2026-03-25 19:30:36 -0300

    fixx

--- PlaySync/backend/demo/src/main/java/com/playsync/demo/config/CorsConfig.java
+++ PlaySync/backend/demo/src/main/java/com/playsync/demo/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", 
+                        "http://127.0.0.1:5174", "http://localhost:3000", "http://136.248.116.43:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
