363d0ca90ebfb0f2c81b80d1169bb499b61ede37
Gabriel Lima de Oliveira <ogabriellima1999@gmail.com>
2026-04-17 00:11:24 -0300

    ajustes

--- PlaySync/backend/demo/src/main/java/com/playsync/demo/repository/ItadMainClassRepository.java
+++ PlaySync/backend/demo/src/main/java/com/playsync/demo/repository/ItadMainClassRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import com.playsync.demo.Entities.ItadMainClass;
 
 @Repository
-public interface ItadMainClassRepository extends CrudRepository<Long, ItadMainClass> {
+public interface ItadMainClassRepository extends CrudRepository<ItadMainClass, Long> {
     @Query("SELECT E FROM ItadMainClass E WHERE E.idGame IN :ids")
     List<ItadMainClass> findByIds(@Param("ids") List<String> ids);
 }
--- PlaySync/backend/demo/src/main/java/com/playsync/demo/service/ItadApiPrecosService.java
+++ PlaySync/backend/demo/src/main/java/com/playsync/demo/service/ItadApiPrecosService.java
@@ -12,13 +12,18 @@ import org.springframework.stereotype.Service;
 import com.playsync.demo.Entities.ItadDeals;
 import com.playsync.demo.Entities.ItadDrm;
 import com.playsync.demo.Entities.ItadMainClass;
+import com.playsync.demo.Entities.ItadPlataforms;
+import com.playsync.demo.Entities.ItadPrice;
+import com.playsync.demo.Entities.ItadRegular;
 import com.playsync.demo.Entities.ItadShop;
 import com.playsync.demo.client.PriceClientItad;
 import com.playsync.demo.dtoresponse.DrmItadResponse;
 import com.playsync.demo.dtoresponse.ItadDealsDto;
 import com.playsync.demo.dtoresponse.ItadMainClassDto;
+import com.playsync.demo.dtoresponse.ItadPlataformsDto;
 import com.playsync.demo.repository.ItadMainClassRepository;
 
+import jakarta.persistence.OneToMany;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -33,7 +38,7 @@ public class ItadApiPrecosService {
         List<ItadMainClass> itadMainClass = this.itadMainClassRepository.findByIds(ids);
 
         if (itadMainClass.isEmpty()) {
-
+            persistDataOfApiInDatabase(callApi(ids));
         }
     }
 
@@ -46,15 +51,56 @@ public class ItadApiPrecosService {
         for (ItadMainClassDto itadMainClassDto : itadMainClassDtos) {
             ItadMainClass itadMainClass = new ItadMainClass(itadMainClassDto.getIdGame(), LocalDateTime.now());
             for (ItadDealsDto itadDealsDto : itadMainClassDto.getDeals()) {
-                ItadDeals itadDeals = new ItadDeals(itadMainClass, itadDealsDto.getCut());
+                ItadDeals itadDeals = new ItadDeals(null, itadDealsDto.getCut());
+                validaExistenciaDeObjetoNoDTOeInsereNoBanco(itadDealsDto, itadDeals);
                 if (!itadDealsDto.getDrm().isEmpty()) {
-                    for (DrmItadResponse drmItadResponse : itadDealsDto.getDrm()) {
-                        ItadDrm itadDrm = new ItadDrm(drmItadResponse.getId(), drmItadResponse.getName(), null);
-                        
-                    }
+                    insereInformacaoDrm(itadDeals, itadDealsDto);
                 }
+                if (!itadDealsDto.getPlataformas().isEmpty()) {
+                    insereInformacaoPlataforms(itadDeals, itadDealsDto);
+                }
+                itadDeals.setItadMainClass(itadMainClass);
+                itadMainClass.getItad_deals().add(itadDeals);
+
             }
+            itadMainClasses.add(itadMainClass);
+        }
+        this.itadMainClassRepository.saveAll(itadMainClasses);
+    }
+
+    private void insereInformacaoDrm(ItadDeals itadDeals, ItadDealsDto itadDealsDto) {
+        for (DrmItadResponse drmItadResponse : itadDealsDto.getDrm()) {
+            ItadDrm itadDrm = new ItadDrm(drmItadResponse.getId(), drmItadResponse.getName(), null);
+            itadDrm.setItadDeals(itadDeals);
+            itadDeals.getItadDrms().add(itadDrm);
         }
     }
 
+    private void insereInformacaoPlataforms(ItadDeals itadDeals, ItadDealsDto itadDealsDto) {
+        for (ItadPlataformsDto itadPlataformsDto : itadDealsDto.getPlataformas()) {
+            ItadPlataforms itadPlataforms = new ItadPlataforms(itadPlataformsDto.getId(),
+                    itadPlataformsDto.getName(), null);
+            itadPlataforms.setItadDeals(itadDeals);
+            itadDeals.getItadPlataforms().add(itadPlataforms);
+        }
+    }
+
+    private void validaExistenciaDeObjetoNoDTOeInsereNoBanco(ItadDealsDto itadDealsDto, ItadDeals itadDeals) {
+        if (itadDealsDto.getPrice() != null) {
+            ItadPrice itadPrice = new ItadPrice(itadDealsDto.getPrice().getAmount(), null);
+            itadPrice.setItadDeals(itadDeals);
+            itadDeals.getItadPrices().add(itadPrice);
+        }
+        if (itadDealsDto.getShop() != null) {
+            ItadShop itadShop = new ItadShop(itadDealsDto.getShop().getId(), itadDealsDto.getShop().getName(),
+                    null);
+            itadShop.setItadDeals(itadDeals);
+            itadDeals.getItadShops().add(itadShop);
+        }
+        if (itadDealsDto.getRegular() != null) {
+            ItadRegular itadRegular = new ItadRegular(itadDealsDto.getRegular().getAmount(), null);
+            itadRegular.setItadDeals(itadDeals);
+            itadDeals.getItadRegulars().add(itadRegular);
+        }
+    }
 }
