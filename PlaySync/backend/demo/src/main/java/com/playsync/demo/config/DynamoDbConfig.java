7a37f74bcd925ad1934b5f098c874d890221d486
Guilherme Aniel <guilherme_a_oliveir2@estudante.sc.senai.br>
2026-04-29 19:47:40 -0300

    feature/add dynamodb

--- PlaySync/backend/demo/src/main/java/com/playsync/demo/config/DynamoDbConfig.java
+++ PlaySync/backend/demo/src/main/java/com/playsync/demo/config/DynamoDbConfig.java
@@ -1,8 +1,11 @@
 package com.playsync.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
@@ -10,10 +13,21 @@ import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 @Configuration
 public class DynamoDbConfig {
 
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.SA_EAST_1)
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKeyId, secretKey)))
                 .build();
     }
 
